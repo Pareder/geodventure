b1e0d4ef7843879c1c5de7225b15569297c89dfb
Pareder <ruskc2beat@gmail.com>
2024-10-11 11:09:50 +0300

    Fix getStreak method, cover with tests

--- src/pages/Profile/__tests__/utils.test.ts
+++ src/pages/Profile/__tests__/utils.test.ts
@@ -1,7 +1,96 @@
 import { getStreak } from '../utils'
 
+function getDate(dayOffset = 0, hours?: number) {
+  const date = new Date()
+  date.setDate(date.getDate() - dayOffset)
+  if (hours !== undefined) {
+    date.setHours(hours)
+  }
+
+  return date
+}
+
 describe('getStreak util', () => {
   it('should return 0 if no games', () => {
     expect(getStreak([])).toBe(0)
+  })
+
+  it('should return 0 if first game is a day before yesterday', () => {
+    expect(getStreak([{ date: getDate(2).getTime() }])).toBe(0)
+  })
+
+  it('should return 0 if first game is today', () => {
+    expect(getStreak([{ date: getDate().getTime() }])).toBe(1)
+  })
+
+  it('should return 0 if first game is yesterday', () => {
+    expect(getStreak([{ date: getDate(1).getTime() }])).toBe(1)
+  })
+
+  it('should return 2 for games between yesterday and today', () => {
+    expect(getStreak([{ date: getDate(0).getTime() }, { date: getDate(1).getTime() }])).toBe(2)
+  })
+
+  it('should return 2 for games between yesterday and today and one more', () => {
+    expect(
+      getStreak([{ date: getDate(0).getTime() }, { date: getDate(1).getTime() }, { date: getDate(3).getTime() }]),
+    ).toBe(2)
+  })
+
+  it('should return 2 for several games between yesterday and today', () => {
+    expect(
+      getStreak([
+        { date: getDate(0).getTime() },
+        { date: getDate(0).getTime() },
+        { date: getDate(1).getTime() },
+        { date: getDate(1).getTime() },
+      ]),
+    ).toBe(2)
+  })
+
+  it('should return 2 for games between a day before yesterday and yesterday', () => {
+    expect(getStreak([{ date: getDate(1).getTime() }, { date: getDate(2).getTime() }])).toBe(2)
+  })
+
+  it('should return 2 for games between a day before yesterday and yesterday and one more', () => {
+    expect(
+      getStreak([{ date: getDate(1).getTime() }, { date: getDate(2).getTime() }, { date: getDate(4).getTime() }]),
+    ).toBe(2)
+  })
+
+  it('should return 2 for several games between a day before yesterday and yesterday', () => {
+    expect(
+      getStreak([
+        { date: getDate(1).getTime() },
+        { date: getDate(1).getTime() },
+        { date: getDate(2).getTime() },
+        { date: getDate(2).getTime() },
+      ]),
+    ).toBe(2)
+  })
+
+  it('should return 0 for games before yesterday', () => {
+    expect(getStreak([{ date: getDate(2).getTime() }, { date: getDate(3).getTime() }])).toBe(0)
+  })
+
+  it('should return 2 for games between yesterday morning and today', () => {
+    expect(getStreak([{ date: getDate(0).getTime() }, { date: getDate(1, 0).getTime() }])).toBe(2)
+  })
+
+  it('should return 2 for games between yesterday noon and today', () => {
+    expect(getStreak([{ date: getDate(0).getTime() }, { date: getDate(1, 23).getTime() }])).toBe(2)
+  })
+
+  it('should return 2 for several games between yesterday morning and today', () => {
+    expect(
+      getStreak([
+        { date: getDate(0, 23).getTime() },
+        { date: getDate(0).getTime() },
+        { date: getDate(0, 0).getTime() },
+        { date: getDate(1, 23).getTime() },
+        { date: getDate(1).getTime() },
+        { date: getDate(1, 0).getTime() },
+      ]),
+    ).toBe(2)
   })
 })
--- src/pages/Profile/utils.ts
+++ src/pages/Profile/utils.ts
@@ -1,34 +1,21 @@
+import dayjs from 'dayjs'
 import { GameType } from 'types'
 
 export function getTotalScore(games: Pick<GameType, 'score'>[]) {
   return games.reduce((acc, game) => acc + game.score, 0)
 }
 
-export function getStreak(games: Pick<GameType, 'date'>[]): number {
-  if (games.length === 0) return 0
+export function getStreak(games: Pick<GameType, 'date'>[]) {
+  let streak = 0
+  for (let i = 0; i < games.length; i++) {
+    const previousDate = dayjs(games[i - 1]?.date).startOf('d')
+    const currentDate = dayjs(games[i].date).startOf('d')
+    const difference = previousDate.diff(currentDate, 'd')
+    if (difference === 1 || (i === 0 && difference === 0)) {
+      streak += 1
+    }
 
-  const today = new Date()
-  today.setHours(0, 0, 0, 0)
-  const yesterday = new Date(today)
-  yesterday.setDate(today.getDate() - 1)
-
-  const firstGameDate = new Date(games[0].date)
-  firstGameDate.setHours(0, 0, 0, 0)
-
-  if (firstGameDate.getTime() < yesterday.getTime()) {
-    return 0
-  }
-
-  let streak = 1
-  for (let i = 1; i < games.length; i++) {
-    const currentDate = new Date(games[i - 1].date)
-    const previousDate = new Date(games[i].date)
-
-    const daysDifference = Math.floor((currentDate.getTime() - previousDate.getTime()) / (1000 * 60 * 60 * 24))
-
-    if (daysDifference === 1) {
-      streak++
-    } else if (daysDifference > 1) {
+    if (difference > 1) {
       break
     }
   }
