8481cc58a8512f24829529b045d3fdc74cf32f56
Pareder <ruskc2beat@gmail.com>
2024-10-16 13:20:42 +0300

    Fix street view to return only outdoor locations

--- src/common/components/StreetMap/utils.ts
+++ src/common/components/StreetMap/utils.ts
@@ -3,23 +3,26 @@ import { getRandomLatLng } from 'common/utils/randomHelpers'
 export function setStreetView(callback?: (coords: google.maps.LatLngLiteral) => void) {
   const randomLatLng = getRandomLatLng()
   const streetViewService = new google.maps.StreetViewService()
-  streetViewService.getPanorama({ location: randomLatLng, radius: 500000 }, (data) => {
-    if (data?.location?.latLng && data.links?.length) {
-      const position = { lat: data.location.latLng.lat(), lng: data.location.latLng.lng() }
-      const element = document.getElementById('street-map')
-      const map = new google.maps.Map(element!, {
-        center: position,
-      })
-      const panorama = new google.maps.StreetViewPanorama(element!, {
-        position,
-        addressControl: false,
-        fullscreenControl: false,
-      })
-      map.setStreetView(panorama)
-      callback?.(position)
-      return
-    }
+  streetViewService.getPanorama(
+    { location: randomLatLng, radius: 500000, sources: [google.maps.StreetViewSource.OUTDOOR] },
+    (data) => {
+      if (data?.location?.latLng && data.links?.length) {
+        const position = { lat: data.location.latLng.lat(), lng: data.location.latLng.lng() }
+        const element = document.getElementById('street-map')
+        const map = new google.maps.Map(element!, {
+          center: position,
+        })
+        const panorama = new google.maps.StreetViewPanorama(element!, {
+          position,
+          addressControl: false,
+          fullscreenControl: false,
+        })
+        map.setStreetView(panorama)
+        callback?.(position)
+        return
+      }
 
-    setStreetView(callback)
-  })
+      setStreetView(callback)
+    },
+  )
 }
